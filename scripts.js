f6e03947f03f01dd16f11db9be7ba9251e267fe9
Marta Szczepaniak <mart.szcz@gmail.com>
2015-03-02 22:51:34 +0100

    Move to keeping data in javascript object. Change the logic of ReRendering basket list.

--- scripts.js
+++ scripts.js
@@ -1,29 +1,89 @@
 $(document).ready(function() {
-  var loadData = $.ajax({
-    url: "products.json",
-    dataType: "json",
-    success: function(data) {
-      data["products"].forEach(function(product) {
-        productListItem = $("<li>")
-        productListItem.text(product["name"])
-        itemLink = $("<a>")
-        itemLink.text("+")
-        productListItem.append(itemLink)
-        $("#products-list").append(productListItem)
-      })
-    }
+  var productsList = {
+    products: [
+      {
+        name: "Butter",
+        id: 0
+      },
+      {
+        name: "Flour",
+        id: 1
+      },
+      {
+        name: "Milk",
+        id: 2
+      },
+      {
+        name: "Eggs",
+        id: 3
+      },
+      {
+        name: "Pasta",
+        id: 4
+      },
+      {
+        name: "Tomato",
+        id: 5
+      },
+      {
+        name: "Cucumber",
+        id: 6
+      },
+      {
+        name: "Orange Juice",
+        id: 7
+      }
+    ]
+  }
+
+  window.productsList = productsList
+
+  var basketList = {
+    products: []
+  }
+
+  window.basketList = basketList
+
+  productsList["products"].forEach(function(product) {
+    productListItem = $("<li>")
+    productListItem.text(product["name"])
+    productListItem.attr("data-id", product["id"])
+    itemLink = $("<a>")
+    itemLink.text("+")
+    productListItem.append(itemLink)
+    $("#products-list").append(productListItem)
   })
 
   $("body").on("click", "#products-list a", function(event) {
-    basketListItem = $("<li>")
-    basketListItem.text($(event.currentTarget).parent().text().slice(0, -1))
-    itemLink = $("<a>")
-    itemLink.text("-")
-    basketListItem.append(itemLink)
-    $("#basket-list").append(basketListItem)
+    productId = parseInt($(event.currentTarget).parent().attr("data-id"))
+    product = _.find(productsList.products, { id: productId })
+    basketList.products.push(product)
+    $("#basket-list").html("")
+    basketList["products"].forEach(function(product) {
+      basketListItem = $("<li>")
+      basketListItem.text(product["name"])
+      basketListItem.attr("data-id", product["id"])
+      itemLink = $("<a>")
+      itemLink.text("-")
+      basketListItem.append(itemLink)
+      $("#basket-list").append(basketListItem)
+    })
   })
 
   $("body").on("click", "#basket-list a", function(event) {
-    $(event.currentTarget).parent().remove()
+    productId = parseInt($(event.currentTarget).parent().attr("data-id"))
+    product = _.find(basketList.products, { id: productId })
+    index = basketList.products.indexOf(product)
+    basketList.products.splice(index, 1)
+    $("#basket-list").html("")
+    basketList["products"].forEach(function(product) {
+      basketListItem = $("<li>")
+      basketListItem.text(product["name"])
+      basketListItem.attr("data-id", product["id"])
+      itemLink = $("<a>")
+      itemLink.text("-")
+      basketListItem.append(itemLink)
+      $("#basket-list").append(basketListItem)
+    })
   })
 })
